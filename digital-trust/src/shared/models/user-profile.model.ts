665906d0951999f110398da91554d148a2c6a230
zisuan <lolkning3225@gmail.com>
2026-03-17 22:06:56 +0800

    removing angular files

--- digital-trust/src/shared/models/user-profile.model.ts
+++
@@ -1,9 +0,0 @@
-
-export interface UserProfile {
-    user_id: string;
-    trust_score: number;
-    spending_distribution: { [key: string]: number };
-    active_hour_heatmap: { [key: string]: number };
-    device_usage_breakdown: { [key: string]: number };
-    location_clusters: Array<{ lat: number; lon: number; transactions: number }>;
-}
